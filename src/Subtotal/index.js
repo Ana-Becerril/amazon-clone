36ef036dbabc189f5a43ca8ecbe4fdd60257e07b
Ana-Becerril <ana.becbel@gmail.com>
2022-06-03 20:34:11 -0500

    added and styled subtotal component on checkout

--- src/Subtotal/index.js
+++ src/Subtotal/index.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react";
+import styles from "./style.module.css";
+import { useStateValue } from "../StateProvider";
+import { Button } from "@material-ui/core";
 
-export default function Subtotal() {
+
+function Subtotal() {
+  const [{ basket }] = useStateValue();
+  const subtotal = basket.reduce(
+    (accumulator, current) => accumulator + current.price,0);
+
   return (
-    <div>Subtotal</div>
-  )
+    <div className={styles.subtotalWrapper}>
+      <p className={styles.subtotalLine}>
+        Subtotal ({basket?.length} items):
+        <span>${subtotal}</span>
+      </p>
+      <div className={styles.checkerGift}>
+        <input type="checkbox" className={styles.checkbox}/>
+        <label className={styles.giftLine}>This orden contains a gift</label>
+      </div>
+      <Button
+        size="small"
+        style={{
+          background: "#f3a847",
+          padding: "5px",
+          borderRadius: "4px",
+          marginTop: "20px",
+        }}
+      >
+        {" "}
+        Proceed to checkout{" "}
+      </Button>
+    </div>
+  );
 }
+
+export default Subtotal;
